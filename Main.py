76e6a9e55af91bb9cc331327c58059ce5a9da8c0
sampreeth65 <55499513+sampreeth65@users.noreply.github.com>
2020-07-07 11:49:30 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,7 +22,6 @@ ballonYCoordinate = 450
 ballonXChange = 0.3
 ballonYChange = 20
 
-# Bullet
 bulletImage = pyg.image.load('bullet.png')
 bulletXCoordinate = random.randint(0,736)
 bulletYCoordinate = 530
@@ -36,15 +35,13 @@ bulletTextXCoordinate = 20
 bulletTextYCoordinate = 20
 
 gameOverText = pyg.font.Font('freesansbold.ttf',64)
-gameOverTextXCoordinate = 200
-gameOverTextYCoordinate = 200
 
-def gameOver(gameOverTextXCoordinate,gameOverTextYCoordinate):
+def gameOver():
     overText = gameOverText.render("GAME OVER",True,(0,0,0))
-    screenWindow.blit(overText,(gameOverTextXCoordinate,gameOverTextYCoordinate))
+    screenWindow.blit(overText,(200,200))
 
 def missedBulletText(xCoordinate, yCoordinate):
-    missed = bulletText.render("Score: " + str(missedBullets), True, (0, 0, 0))
+    missed = bulletText.render("Bullets: " + str(missedBullets), True, (0, 0, 0))
     screenWindow.blit(missed,(xCoordinate,yCoordinate))
 
 def player(xCoordinate, yCoordinate):
@@ -58,8 +55,8 @@ def fireBullet(xCoordinate, yCoordinate):
     bullet_state = 'fire'
     screenWindow.blit(bulletImage,(xCoordinate + 16,yCoordinate + 10))
 
-def isCollision(enemyXCoordinate,enemyYCoordinate,bulletXCoordinate,bulletYCoordinate):
-    distance = math.sqrt(math.pow(enemyXCoordinate - bulletXCoordinate,2) + math.pow(enemyYCoordinate - bulletYCoordinate,2))
+def isCollision(ballonXCoordinate,ballonYCoordinate,bulletXCoordinate,bulletYCoordinate):
+    distance = math.sqrt(math.pow(ballonXCoordinate - bulletXCoordinate,2) + math.pow(ballonYCoordinate - bulletYCoordinate,2))
     if distance <= 27:
         return True
     return False
@@ -69,8 +66,6 @@ def gameOverCollision(ballXCoordinate,ballYCoordinate,playerXCoordinate,playerYC
     if distance <= 50:
         return True
     return False
-
-
 
 screenOpen = True
 while screenOpen:
@@ -89,8 +84,6 @@ while screenOpen:
                     bulletXCoordinate = cannonXCoordinate
                     fireBullet(bulletXCoordinate, bulletYCoordinate)
 
-
-
         if event.type == pyg.KEYUP:
             if event.key == pyg.K_RIGHT or event.key == pyg.K_LEFT:
                 cannonXChanges = 0
@@ -104,8 +97,8 @@ while screenOpen:
 
     ballonXCoordinate += ballonXChange
 
-
-
+    overCollision = gameOverCollision(ballonXCoordinate, ballonYCoordinate, cannonXCoordinate, cannonYCoordinate)
+    collision = isCollision(ballonXCoordinate, ballonYCoordinate, bulletXCoordinate, bulletYCoordinate)
 
     if ballonXCoordinate <= 0:
         ballonXChange = 0.3
@@ -123,28 +116,13 @@ while screenOpen:
         fireBullet(bulletXCoordinate, bulletYCoordinate)
         bulletYCoordinate -= bulletYChange
 
-    collision = isCollision(ballonXCoordinate, ballonYCoordinate, bulletXCoordinate, bulletYCoordinate)
-
-    """
-    if collision:
-        bulletYCoordinate = 530
-        bullet_state = 'ready'
-        missedBullets += 1
-        # print(playerScore)
-        ballonXCoordinate = random.randint(0, 736)
-        ballonYCoordinate = 0
-    """
-
-    overCollision = gameOverCollision(ballonXCoordinate, ballonYCoordinate, cannonXCoordinate, cannonYCoordinate)
-    if collision or overCollision:
+    if overCollision:
+        gameOver()
         while True:
             bulletYCoordinate = 530
             bullet_state = 'ready'
-            ballonYCoordinate = 2000
-            gameOver(gameOverTextXCoordinate, gameOverTextYCoordinate)
+            ballonYCoordinate = 1500
             break
-
-
 
     ballon(ballonXCoordinate, ballonYCoordinate)
     player(cannonXCoordinate, cannonYCoordinate)
