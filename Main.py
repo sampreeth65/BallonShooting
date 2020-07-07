73908d9c8213c82ca8d095e35bf60cbe08331872
sampreeth65 <55499513+sampreeth65@users.noreply.github.com>
2020-07-07 11:12:49 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,33 +10,50 @@ pyg.display.set_caption("Ballon Shooting")
 icon = pyg.image.load('Icon.png')
 pyg.display.set_icon(icon)
 
-playerScore = 0
-playerImage = pyg.image.load('player.png')
-playerXCoordinate = 370
-playerYCoordinate = 530
-playerXChanges = 0
 
-enemyImage = pyg.image.load('balloon.png')
-enemyXCoordinate = random.randint(0,736)
-enemyYCoordinate = 0
-enemyXChange = 0.3
-enemyYChange = 20
+cannonImage = pyg.image.load('player.png')
+cannonXCoordinate = 370
+cannonYCoordinate = 530
+cannonXChanges = 0
+
+ballonImage = pyg.image.load('balloon.png')
+ballonXCoordinate = random.randint(0, 736)
+ballonYCoordinate = 450
+ballonXChange = 0.3
+ballonYChange = 20
 
 # Bullet
 bulletImage = pyg.image.load('bullet.png')
 bulletXCoordinate = random.randint(0,736)
 bulletYCoordinate = 530
 bulletXChange = 0
-bulletYChange = 0.5
+bulletYChange = 1
 bullet_state = "ready"
 
+missedBullets = 0
+bulletText = pyg.font.Font('freesansbold.ttf', 32)
+bulletTextXCoordinate = 20
+bulletTextYCoordinate = 20
+
+gameOverText = pyg.font.Font('freesansbold.ttf',64)
+gameOverTextXCoordinate = 200
+gameOverTextYCoordinate = 200
+
+def gameOver(gameOverTextXCoordinate,gameOverTextYCoordinate):
+    overText = gameOverText.render("GAME OVER",True,(0,0,0))
+    screenWindow.blit(overText,(gameOverTextXCoordinate,gameOverTextYCoordinate))
+
+def missedBulletText(xCoordinate, yCoordinate):
+    missed = bulletText.render("Score: " + str(missedBullets), True, (0, 0, 0))
+    screenWindow.blit(missed,(xCoordinate,yCoordinate))
+
 def player(xCoordinate, yCoordinate):
-    screenWindow.blit(playerImage, (xCoordinate, yCoordinate))
+    screenWindow.blit(cannonImage, (xCoordinate, yCoordinate))
 
-def enemy(xCoordinate,yCoordinate):
-    screenWindow.blit(enemyImage,(xCoordinate,yCoordinate))
+def ballon(xCoordinate, yCoordinate):
+    screenWindow.blit(ballonImage, (xCoordinate, yCoordinate))
 
-def fire_bullet(xCoordinate,yCoordinate):
+def fireBullet(xCoordinate, yCoordinate):
     global bullet_state
     bullet_state = 'fire'
     screenWindow.blit(bulletImage,(xCoordinate + 16,yCoordinate + 10))
@@ -44,6 +61,12 @@ def fire_bullet(xCoordinate,yCoordinate):
 def isCollision(enemyXCoordinate,enemyYCoordinate,bulletXCoordinate,bulletYCoordinate):
     distance = math.sqrt(math.pow(enemyXCoordinate - bulletXCoordinate,2) + math.pow(enemyYCoordinate - bulletYCoordinate,2))
     if distance <= 27:
+        return True
+    return False
+
+def gameOverCollision(ballXCoordinate,ballYCoordinate,playerXCoordinate,playerYCoordinate):
+    distance = math.sqrt(math.pow(ballXCoordinate - playerXCoordinate,2) + math.pow(ballYCoordinate - playerYCoordinate,2))
+    if distance <= 50:
         return True
     return False
 
@@ -58,55 +81,72 @@ while screenOpen:
 
         if event.type == pyg.KEYDOWN:
             if event.key == pyg.K_LEFT:
-                playerXChanges = -0.5
+                cannonXChanges = -0.5
             if event.key == pyg.K_RIGHT:
-                playerXChanges = 0.5
+                cannonXChanges = 0.5
             if event.key == pyg.K_SPACE:
                 if bullet_state is 'ready':
-                    bulletXCoordinate = playerXCoordinate
-                    fire_bullet(bulletXCoordinate,bulletYCoordinate)
+                    bulletXCoordinate = cannonXCoordinate
+                    fireBullet(bulletXCoordinate, bulletYCoordinate)
+
 
 
         if event.type == pyg.KEYUP:
             if event.key == pyg.K_RIGHT or event.key == pyg.K_LEFT:
-                playerXChanges = 0
+                cannonXChanges = 0
 
 
-    playerXCoordinate += playerXChanges
-    if playerXCoordinate <= 0:
-        playerXCoordinate = 0
-    elif playerXCoordinate > 736:
-        playerXCoordinate = 736
+    cannonXCoordinate += cannonXChanges
+    if cannonXCoordinate <= 0:
+        cannonXCoordinate = 0
+    elif cannonXCoordinate > 736:
+        cannonXCoordinate = 736
 
-    enemyXCoordinate += enemyXChange
+    ballonXCoordinate += ballonXChange
 
-    if enemyXCoordinate <= 0:
-        enemyXChange = 0.3
-        enemyYCoordinate += enemyYChange
-    elif enemyXCoordinate > 736:
-        enemyXChange = -0.3
-        enemyYCoordinate += enemyYChange
+
+
+
+    if ballonXCoordinate <= 0:
+        ballonXChange = 0.3
+        ballonYCoordinate += ballonYChange
+    elif ballonXCoordinate > 736:
+        ballonXChange = -0.3
+        ballonYCoordinate += ballonYChange
 
     if bulletYCoordinate <= 0:
+        missedBullets += 1
         bulletYCoordinate = 530
         bullet_state = 'ready'
 
     if bullet_state is 'fire':
-        fire_bullet(bulletXCoordinate,bulletYCoordinate)
+        fireBullet(bulletXCoordinate, bulletYCoordinate)
         bulletYCoordinate -= bulletYChange
 
-    collision = isCollision(enemyXCoordinate,enemyYCoordinate,bulletXCoordinate,bulletYCoordinate)
+    collision = isCollision(ballonXCoordinate, ballonYCoordinate, bulletXCoordinate, bulletYCoordinate)
+
+    """
     if collision:
         bulletYCoordinate = 530
         bullet_state = 'ready'
-        playerScore += 1
+        missedBullets += 1
         # print(playerScore)
-        enemyXCoordinate = random.randint(0, 736)
-        enemyYCoordinate = 0
+        ballonXCoordinate = random.randint(0, 736)
+        ballonYCoordinate = 0
+    """
+
+    overCollision = gameOverCollision(ballonXCoordinate, ballonYCoordinate, cannonXCoordinate, cannonYCoordinate)
+    if collision or overCollision:
+        while True:
+            bulletYCoordinate = 530
+            bullet_state = 'ready'
+            ballonYCoordinate = 2000
+            gameOver(gameOverTextXCoordinate, gameOverTextYCoordinate)
+            break
 
 
 
-    enemy(enemyXCoordinate,enemyYCoordinate)
-    player(playerXCoordinate, playerYCoordinate)
-
+    ballon(ballonXCoordinate, ballonYCoordinate)
+    player(cannonXCoordinate, cannonYCoordinate)
+    missedBulletText(bulletTextXCoordinate, bulletTextYCoordinate)
     pyg.display.update()
